2b491b0b5d6c5ee0c6ec37eec2fea8ce49f4c97b
sonnypetersen <sonny.petersen@gmail.com>
2021-03-03 15:45:57 +0100

    Update async_await.js

--- async_await.js
+++ async_await.js
@@ -2,21 +2,26 @@ async function getPosts() {
     const postsPromise = await fetch(
         "https://jsonplaceholder.typicode.com/posts"
     );
-    const posts = await postsPromise.json();
-    let html = "";
+    
+    if(postsPromise.ok) {
+        const posts = await postsPromise.json();
+        let html = "";
 
-    posts.slice(0, 5).forEach((post) => { 
-        const title = post.title;
-        const body = post.body;
-        html += `
-        <div class='post'>
-            <h3>${title}</h3>
-            <p>${body}</p>
-        </div>
-        `;
-    });
+        posts.slice(0, 5).forEach((post) => { 
+            const title = post.title;
+            const body = post.body;
+            html += `
+            <div class='post'>
+                <h3>${title}</h3>
+                <p>${body}</p>
+            </div>
+            `;
+        });
 
-    document.body.innerHTML = html;
+        document.body.innerHTML = html;
+    } else {
+        console.error(`Error: ${postsPromise.status}`)
+    }
 }
 
 getPosts();
