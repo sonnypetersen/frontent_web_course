d90ca972c30eaf042f9b29d853f9116e908cd679
sonnypetersen <sonny.petersen@gmail.com>
2021-03-03 15:59:33 +0100

    Create async_await_POST.js

---
+++ async_await_POST.js
@@ -0,0 +1,36 @@
+document.getElementById("fetchForm").addEventListener("submit", submitPost);
+
+async function submitPost(e) {
+    e.preventDefault();
+
+    let title = document.getElementById("titleInput").value;
+    let body = document.getElementById("bodyInput").value;
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify({ title: title, body: body }),
+        headers: new Headers({
+        "Content-Type": "application/json"
+        })
+    };
+
+    const postPromise = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        options
+    );
+  
+    if(postPromise.ok) {
+        const post = await postPromise.json();
+
+        title = post.title;
+        body = post.body;
+    } else {
+        title = 'Error';
+        body = `Status: ${postPromise.status}`;
+    }
+  
+    document.querySelector(".card-title").innerText = title;
+    document.querySelector(".card-text").innerText = body;
+
+    document.getElementById("fetchForm").reset();
+}
